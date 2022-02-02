a9e0ef3d463fbc63b67e81cd091607dbdddf7f8e
i.poroshin <i.poroshin@uwca.ru>
2022-02-02 09:02:48 +0500

    delete files

--- server/session.cpp
+++
@@ -1,9 +0,0 @@
-#include "session.h"
-#include "sessionManager.h"
-
-
-void session::closeSession()
-{
-    auto self = shared_from_this();
-    m_manager.erase(shared_from_this());
-}
--- server/sessionManager.cpp
+++
@@ -1,8 +0,0 @@
-#include "sessionManager.h"
-#include "session.h"
-pSession sessionManager::newSession()
-{
-     return std::make_shared<session>(m_context, *this);
-}
-
-
